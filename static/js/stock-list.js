17aa23e0b0f2b3c50467a61f3c01c5a4949fe78b
Kevyn <532775134@qq.com>
2021-10-15 14:36:49 +0800

    v0.9.7

--- static/js/stock-list.js
+++ static/js/stock-list.js
@@ -3,19 +3,42 @@
         delimiters: ['[[', ']]'],
         data() {
             return {
-                stock_list: []
+                page: 1,
+                stockDisplayInAPage: 30,
+                stockList: [],
+                displayStockList: [],
+                searchString: ''
             };
         },
         mounted() {
             let page = 1;
             axios.get(`/stock-list-data?exchange=sh&page=${page}`)
                 .then((response) => {
-                    response.data['stocks'].length = 100;
-                    this.stock_list.push(response.data['stocks']);
+                    for (let i = 0; i < response.data['stocks'].length; i++)
+                        this.stockList.push(response.data['stocks'][i]);
+                    this.displayStockList = this.stockList.slice((this.page-1)*this.stockDisplayInAPage, this.page*this.stockDisplayInAPage-1),
                     changeContainerHeight('');
                 }).catch(function (error) {
                     console.log(error);
                 });
+        },
+        methods: {
+            search() {
+                window.open(`/stock-quotation?stock=${this.searchString}`, '_self')
+            },
+            changePage(ind, event=null) {
+                if (ind == 0) {
+                    this.page = event.currentTarget.value;
+                }
+                else if (ind == -1) {
+                    this.page = this.page == 1 ? 10 : this.page - 1;
+                }
+                else if (ind == 1) {
+                    this.page = this.page == 10 ? 1 : this.page + 1;
+                }
+                console.log(this.page)
+                this.displayStockList = this.stockList.slice((this.page-1)*this.stockDisplayInAPage, this.page*this.stockDisplayInAPage-1);
+            }
         }
     }
     Vue.createApp(stockListBean).mount(".central-box");
