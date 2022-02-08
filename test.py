2eb28169b8a898fbb0cbddba9e4e0c2b50a85307
Kevyn <532775134@qq.com>
2022-02-08 19:38:51 +0800

    remove temp.txt

--- test.py
+++ test.py
@@ -2,9 +2,8 @@ from data_source import local_source
 
 if __name__ == '__main__':
     print(local_source.get_quotations_daily(
-        cols='TS_CODE, CHANGE',
-        condition='TRADE_DATE = "20220207" ORDER BY CHANGE DESC'
+        condition='TRADE_DATE = "20220208" AND TS_CODE = "603123.SH"'
     ))
     print(local_source.get_stock_list(
-        condition='TS_CODE = "688696.SH"'
+        condition='TS_CODE = "603123.SH"'
     ))
